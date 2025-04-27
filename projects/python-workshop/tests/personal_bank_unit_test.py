3391c59e85af964d7d079f1129453ca55bab33b9
Mikerniker <watkins.michelle@gmail.com>
2025-04-28 03:02:40 +0800

    fix instance error

--- projects/python-workshop/tests/personal_bank_unit_test.py
+++ projects/python-workshop/tests/personal_bank_unit_test.py
@@ -11,7 +11,7 @@ def context() -> Iterator[AlgopyTestContext]:
 
 def test_deposit(context: AlgopyTestContext) -> None:
     #Arrange
-    contract = PersonalBank
+    contract = PersonalBank()
     app = context.ledger.get_app(contract)
     deposit_amount = context.any.uint64()
     deposit_txn = context.any.txn.payment(receiver=app.address, amount=deposit_amount)
